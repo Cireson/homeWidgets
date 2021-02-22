0f953720c09f3fb2880e6f5e04b752378543ec93
Justin Workman <37841550+justinkwork@users.noreply.github.com>
2021-02-22 17:15:22 -0600

    Delete link-replace.js

--- link-replace.js
+++
@@ -1,5 +0,0 @@
-app.events.subscribe('homeWidgetsReady', function(data, homeWidgets){
-	homeWidgets.add('link-replace', function(widget){
-		$(widget).parent().parent().parent().parent().prev().remove();
-	});
-});
